3d001abd3bb54344d2a1dd0a32e7d9f3683e5459
zhangfengzhou <1120335370@qq.com>
2020-02-13 18:51:16 +0800

    我的信息页面调整完成

--- app/src/main/java/com/luckyboy/jetpacklearn/ui/fragment/main/MeFragment.kt
+++ app/src/main/java/com/luckyboy/jetpacklearn/ui/fragment/main/MeFragment.kt
@@ -1,20 +1,36 @@
 package com.luckyboy.jetpacklearn.ui.fragment.main
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.luckyboy.jetpacklearn.R
+import com.luckyboy.jetpacklearn.common.BaseConstant
+import com.luckyboy.jetpacklearn.databinding.ActivityUserAvatarBinding
 import com.luckyboy.jetpacklearn.databinding.FragmentMeBinding
+import com.luckyboy.jetpacklearn.viewmodel.CustomViewModelProvider
+import com.luckyboy.jetpacklearn.viewmodel.MeModel
 
 class MeFragment : Fragment() {
 
     private val TAG by lazy {
         this::class.java.simpleName
     }
+
+    private val model:MeModel by viewModels{
+        CustomViewModelProvider.providerMeModel(requireContext())
+    }
+
 
     private lateinit var meBinding: FragmentMeBinding
 
@@ -25,6 +41,8 @@ class MeFragment : Fragment() {
     ): View? {
         Log.e(TAG, "onCreateView")
         meBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_me, container, false)
+        initListener(meBinding)
+        onSubscribeUi(meBinding)
         return meBinding.root
     }
 
@@ -47,6 +65,79 @@ class MeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         Log.e(TAG, "onDestroyView")
+    }
+
+    // 选择图片的标识
+    private val REQUEST_CODE_IMAGE = 100
+    // 加载框
+    private val sweetAlertDialog: SweetAlertDialog by lazy {
+        SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE)
+            .setTitleText("头像")
+            .setContentText("更新中...")
+    }
+
+    // 初始化监听器
+    private fun initListener(binding: FragmentMeBinding) {
+        binding.ivAvatar.setOnClickListener {
+            // 选择处理的图片
+            val chooseIntent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(chooseIntent, REQUEST_CODE_IMAGE)
+        }
+    }
+
+    // Binding 绑定
+    private fun onSubscribeUi(binding: FragmentMeBinding) {
+        model.user.observe(this, Observer {
+            binding.user = it
+        })
+        // 任务状态的观测
+        model.outPutWorkInfos.observe(this, Observer {
+            if (it.isNullOrEmpty()) {
+                return@Observer
+            }
+            val state = it[0]
+            if (state.state.isFinished) {
+                // 更新头像
+                Log.e(TAG, "获取到头像数据后 准备更新头像操作")
+                val outputImageUri = state.outputData.getString(BaseConstant.KEY_IMAGE_URI)
+                if (!outputImageUri.isNullOrEmpty()) {
+                    model.setOutputUri(outputImageUri)
+                }
+                sweetAlertDialog.dismiss()
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_IMAGE -> data?.let {
+                    handleImageRequestResult(data)
+                }
+            }
+        } else {
+            Log.e(TAG, String.format("Unexpected Result code %s", resultCode))
+        }
+    }
+
+    // 图片选择完成的处理
+    private fun handleImageRequestResult(intent: Intent) {
+        val imageUri: Uri? = intent.clipData?.let {
+            it.getItemAt(0).uri
+        } ?: intent.data
+        Log.e(TAG, "handleImageRequestResult $imageUri")
+        if (imageUri == null) {
+            Log.e(TAG, "Invalid input image uri")
+            return
+        }
+        sweetAlertDialog.show()
+        // 图片模糊处理
+        model.setImageUri(imageUri.toString())
+        model.applyBlur(3)
     }
 
 
